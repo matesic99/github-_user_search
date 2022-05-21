3459276fecb5c6d44382c91a20128a8b358c2940
matesic99 <matesic@arch.localdomain>
2022-05-21 21:12:35 +0200

    second commit

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
-
+const image = document.getElementById("image")
 const user = document.getElementById("name");
 const git_link = document.getElementById("link")
 const joined = document.getElementById("joined")
+const gitBio = document.getElementById("bio");
 const repo = document.getElementById("repos");
 const follower = document.getElementById("followers");
 const followings = document.getElementById("following");
@@ -11,36 +12,45 @@ const locations = document.getElementById("location");
 const twit = document.getElementById("twiter");
 const websites = document.getElementById("website");
 const companies = document.getElementById("company");
-const gitBio = document.getElementById("bio");
 
 
-btn.addEventListener("click", function () {
-    const url = (`https://api.github.com/users/${input.value}`);
+
+const searchUser = ()=> {
+    const url =`https://api.github.com/users/${input.value}`;
     async function getUrl() {
       const response = await fetch(url);
       const data = await response.json();
       const dateData = data.created_at.slice(0, data.created_at.length - 10);
 
-      user.innerHTML = data.name;
-      login.innerHTML = `@${data.login}`;
+      image.src = data.avatar_url
+      
+      user.innerHTML = data.name === "" || data.name === null
+      ? "No Name"
+      : data.name;;
+   
       joined.innerHTML = `Joined ${dateData}`;
-      repo.innerHTML = data.public_repos;
-      follower.innerHTML = data.followers;
-      followings.innerHTML = data.following;
+      repo.textContent = data.public_repos;
+      follower.textContent = data.followers;
+      followings.textContent = data.following;
+
       locations.innerHTML =
         data.location === "" || data.location === null
           ? "No Location"
           : data.location;
+
       twit.innerHTML =
         data.twitter_username === "" || data.twitter_username === null
           ? "No Twitter"
           : data.twitter_username;
+
       websites.innerHTML =
         data.blog === "" || data.blog === null ? "No Website" : data.blog;
+
       companies.innerHTML =
         data.company === "" || data.company === null
           ? "No Company"
           : data.company;
+
       gitBio.innerHTML =
         data.bio === "" || data.bio === null
           ? "This profile has no bio..."
@@ -48,5 +58,7 @@ btn.addEventListener("click", function () {
     }
     getUrl();
     input.value = "";
-  });
+  };
+
+  btn.addEventListener("click", searchUser)
   
